31d00a4fa9f19d20bb698c1089bda267a169a105
Brandon Satrom <bsatrom@gmail.com>
2018-07-05 16:17:31 -0500

    initial commit of skeleton

---
+++ .vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  base: '/content/',
+  themeConfig: {
+    repo: 'particle-iot/workshops',
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Team', link: 'http://particle.io' }
+    ],
+    sidebar: [
+      '/content/',
+      ['/content/ch1', 'Chapter 1: Claiming your Device'],
+      ['/content/ch2', 'Chapter 2: Sense from your Badge & Control a Display'],
+      ['/content/ch3', 'Chapter 3: More sensors and actuators'],
+      ['/content/ch4', 'Chapter 4: Controlling your badge from the internet!'],
+      ['/content/extra1', 'Extra: Taking your Photon exploration further']
+    ]
+  },
+  title: 'Particle Workshops',
+  description:
+    'Workshops designed to teach the basics of IoT development with the Particle ecosystem'
+};
